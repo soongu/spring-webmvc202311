9f434240e07007627b0c9fb828e8a18cedea1405
soongu <naong526@naver.com>
2023-12-27 09:39:39 +0900

    #36. 인터셉터 기초

---
+++ src/main/java/com/spring/mvc/config/InterceptorConfig.java
@@ -0,0 +1,25 @@
+package com.spring.mvc.config;
+
+import com.spring.mvc.interceptor.AfterLoginInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// 만든 인터셉터들을 스프링 컨텍스트에 등록하는 설정 파일
+@Configuration
+@RequiredArgsConstructor
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    private final AfterLoginInterceptor afterLoginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        // 로그인 후 비회원전용페이지 접근 차단 인터셉터 설정
+        registry
+                .addInterceptor(afterLoginInterceptor) // 어떤 인터셉터를 등록할 것인가
+                .addPathPatterns("/members/sign-in", "/members/sign-up") // 어떤 요청에서 인터셉터를 발동시킬 것인가
+                ;
+    }
+}
