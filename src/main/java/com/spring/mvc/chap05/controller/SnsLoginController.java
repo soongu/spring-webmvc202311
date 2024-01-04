fa6ddcf17c29179c2dc5b6e17472d41654d81a47
soongu <naong526@naver.com>
2024-01-04 11:37:26 +0900

    #51. 카카오 로그인 처리 완료

--- src/main/java/com/spring/mvc/chap05/controller/SnsLoginController.java
+++ src/main/java/com/spring/mvc/chap05/controller/SnsLoginController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,7 +37,7 @@ public class SnsLoginController {
 
     // 인가 코드 받기
     @GetMapping("/auth/kakao")
-    public String snsKakao(String code) {
+    public String snsKakao(String code, HttpSession session) {
         log.info("카카오 로그인 인가 코드: {}", code);
 
         // 인가 코드를 가지고 카카오 인증서버에 토큰발급요청을 보냄
@@ -46,9 +47,9 @@ public class SnsLoginController {
         params.put("redirect", kakaoRedirectUri);
         params.put("code", code);
 
-        snsLoginService.kakaoLogin(params);
+        snsLoginService.kakaoLogin(params, session);
 
-        return "redirect:/members/sign-in";
+        return "redirect:/";
     }
 
 }
