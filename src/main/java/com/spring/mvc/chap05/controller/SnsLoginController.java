152cb90995830c11245a0695d0531940775a607b
soongu <naong526@naver.com>
2024-01-04 10:52:21 +0900

    #50. 카카오 로그인 토큰 발급 받기

--- src/main/java/com/spring/mvc/chap05/controller/SnsLoginController.java
+++ src/main/java/com/spring/mvc/chap05/controller/SnsLoginController.java
@@ -1,13 +1,21 @@
 package com.spring.mvc.chap05.controller;
 
+import com.spring.mvc.chap05.service.SnsLoginService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class SnsLoginController {
+
+    private final SnsLoginService snsLoginService;
 
     @Value("${sns.kakao.app-key}")
     private String kakaoAppKey;
@@ -30,6 +38,16 @@ public class SnsLoginController {
     @GetMapping("/auth/kakao")
     public String snsKakao(String code) {
         log.info("카카오 로그인 인가 코드: {}", code);
+
+        // 인가 코드를 가지고 카카오 인증서버에 토큰발급요청을 보냄
+        // server to server 통신
+        Map<String, String> params = new HashMap<>();
+        params.put("appkey", kakaoAppKey);
+        params.put("redirect", kakaoRedirectUri);
+        params.put("code", code);
+
+        snsLoginService.kakaoLogin(params);
+
         return "redirect:/members/sign-in";
     }
 
